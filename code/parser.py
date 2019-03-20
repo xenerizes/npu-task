f0c039e593f476dbf9bfce6aae81223d76295324
Yuliya Skobtsova <xenerizes@users.noreply.github.com>
2019-03-20 12:26:10 +0300

    grammar for parser section

--- code/parser.py
+++ code/parser.py
@@ -1,13 +1,12 @@
-from .base import BaseLexer
+from .base import BaseLexer, BaseParser
 
 
 class ParserLexer(BaseLexer):
     # Reserved tokens
     reserved = {
-        'parser': 'PRS',
+        'parser': 'PARSE',
         'PHV': 'PHV',
         'HEADER': 'HEADER',
-        'PORTMASK': 'PORTMASK',
         'store': 'STORE',
         'mov': 'MOV',
         'cmpje': 'CMPJE',
@@ -24,3 +23,73 @@ class ParserLexer(BaseLexer):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
         t.type = ParserLexer.reserved.get(t.value, 'ID')  # Check for reserved words
         return t
+
+
+class ParserParser(BaseParser):
+    tokens = ParserLexer.tokens
+    start = 'parser'
+
+    def p_parser(self, p):
+        'parser : PARSE parse_code'
+
+    def p_parse_code(self, p):
+        '''parse_code : empty
+                      | instructions parse_code'''
+
+    def p_instructions(self, p):
+        '''instructions : store
+                        | mov
+                        | cmpje
+                        | cmpjn
+                        | j
+                        | label'''
+
+    def p_store(self, p):
+        'store : STORE phv COMMA header COMMA INT'
+
+    def p_mov(self, p):
+        'mov : MOV regmem COMMA allval COMMA INT'
+
+    def p_cmpje(self, p):
+        'cmpje : CMPJE reg COMMA number COMMA label_id'
+
+    def p_cmpjn(self, p):
+        'cmpjn : CMPJN reg COMMA number COMMA label_id'
+
+    def p_j(self, p):
+        'j : J label'
+
+    def p_label(self, p):
+        'label : label_id COLON'
+
+    def p_label_id(self, p):
+        '''label_id : ID
+                    | HALT'''
+
+    def p_reg(self, p):
+        '''reg : R1
+               | R2'''
+
+    def p_phv(self, p):
+        'phv : PHV shift'
+
+    def p_header(self, p):
+        'header : HEADER shift'
+
+    def p_regmem(self, p):
+        '''regmem : reg
+                  | phv'''
+
+    def p_allval(self, p):
+        '''allval : reg
+                  | number
+                  | phv'''
+
+    def p_shift(self, p):
+        '''shift : empty
+                 | PLUS INT'''
+
+    def p_number(self, p):
+        '''number : INT
+                  | HEX'''
+        p[0] = p[1]
