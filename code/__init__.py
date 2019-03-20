a867bdcda2a7b44baadaf712e7f13a9e6ab91c5e
Yuliya Skobtsova <xenerizes@users.noreply.github.com>
2019-03-20 15:11:59 +0300

    modules=>strings'

--- code/__init__.py
+++ code/__init__.py
@@ -2,4 +2,4 @@ from .parser import ParserParser
 from .match_action import MatchActionParser
 from .deparser import DeparserParser
 
-__all__ = [ParserParser, MatchActionParser, DeparserParser]
+__all__ = ['ParserParser', 'MatchActionParser', 'DeparserParser']
