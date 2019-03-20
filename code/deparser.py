5ab752706e29c192aedfcab72f531a067a297d03
Yuliya Skobtsova <xenerizes@users.noreply.github.com>
2019-03-20 13:59:05 +0300

    grammar for deparser

--- code/deparser.py
+++ code/deparser.py
@@ -1,4 +1,4 @@
-from .base import BaseLexer
+from .base import BaseLexer, BaseParser
 
 
 class DeparserLexer(BaseLexer):
@@ -16,3 +16,33 @@ class DeparserLexer(BaseLexer):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
         t.type = DeparserLexer.reserved.get(t.value, 'ID')  # Check for reserved words
         return t
+
+
+class DeparserParser(BaseParser):
+    tokens = DeparserLexer.tokens
+    start = 'deparser'
+
+    def p_deparser(self, p):
+        'deparser : DPRS code'
+
+    def p_code(self, p):
+        '''code : empty
+                | load code'''
+
+    def p_load(self, p):
+        'load : LOAD header COMMA phv COMMA INT'
+
+    def p_phv(self, p):
+        'phv : PHV shift'
+
+    def p_header(self, p):
+        'header : HEADER shift'
+
+    def p_shift(self, p):
+        '''shift : empty
+                 | PLUS number'''
+
+    def p_number(self, p):
+        '''number : INT
+                  | HEX'''
+        p[0] = p[1]
