8b143e6d5bd52ec1bec781d16d5b2fe2822be2d1
Yuliya Skobtsova <xenerizes@users.noreply.github.com>
2019-03-20 15:12:12 +0300

    application

--- application/app.py
+++ application/app.py
@@ -1,6 +1,44 @@
+from argparse import ArgumentParser, FileType
+from code import *
+
+
+def make_parser():
+    parser = ArgumentParser(description='NPU model')
+    parser.add_argument('asm', metavar='ASM', type=str,
+                        help='filename with assembler code')
+    return parser
+
+
 class Application(object):
     def __init__(self, args):
-        pass
+        parser = make_parser()
+        self.args = parser.parse_args(args)
+        self.asm = None
+        self.syntax = None
 
     def run(self):
-        pass
+        self.asm = self.__read_asm()
+        self.syntax = self.__split()
+        for data, parser in self.syntax.items():
+            parser.build()
+            parser.parse(data)
+
+    def __read_asm(self):
+        with open(self.args.asm, 'r') as f:
+            f.seek(0)
+            data = f.readlines()
+            return " ".join(data).replace('\n', ' ')
+
+    def __split(self):
+        lst = self.asm.split('.')
+        parser_data = [x for x in lst if x.startswith('parser')]
+        ma_data = [x for x in lst if x.startswith('match_action')]
+        deparser_data = [x for x in lst if x.startswith('deparser')]
+        if len(parser_data) != 1 or len(ma_data) < 1 or len(deparser_data) != 1:
+            raise SyntaxError("At least one section of each type .parser, "
+                              ".match-action and .deparser are required")
+        return {
+            " ".join(parser_data): ParserParser(),
+            " ".join(ma_data): MatchActionParser(),
+            " ".join(deparser_data): DeparserParser()
+        }
