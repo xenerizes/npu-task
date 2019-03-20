eb16cabde9d5b8f6e1145b28fe48938ac8d7eb4a
Yuliya Skobtsova <xenerizes@users.noreply.github.com>
2019-03-20 12:46:06 +0300

    updated l2sw example

--- examples/l2sw.asm
+++ examples/l2sw.asm
@@ -1,23 +1,23 @@
 .parser
     store PHV, HEADER, 12
-    mov r1, PHV+6, 6
+    mov r1, PHV, 6
     cmpje r1, 0xffffffffffff, halt
 
 .match_action 1
     mov r1, PHV+6, 6
     call exact_match
     cmpje r1, 1, done
-    ctrl
+    or PORTMASK, 0x80
 done:
 
 .match_action 2
-    mov r1, PHV+12, 6
+    mov r1, PHV+6, 6
     call exact_match
     cmpje r2, 1, not_found
-    ori PORTMASK, r1
+    or PORTMASK, r1
     j done
 not_found:
-    mov PORTMASK, -1
+    or PORTMASK, 0xff
 done:
 
 .deparser
