429a49b40581f822363e26b22b53df74f2d46a9e
Yuliya <xenerizes@users.noreply.github.com>
2019-03-23 01:53:05 +0300

    __str__ method in context for logging

--- model/meta.py
+++ model/meta.py
@@ -5,9 +5,21 @@ def _split_header(packet):
     return [b for b in packet[:HEADER_LEN]]
 
 
+def _bytestr(intarray):
+    return bytes(bytearray(intarray))
+
+
 class Context(object):
     def __init__(self, packet, header=None, phv=None, portmask=None):
         self.packet = packet
         self.header = _split_header(packet) if header is None else header
         self.phv = [0] * PHV_LEN if phv is None else phv
         self.portmask = 0x00 if portmask is None else portmask
+
+    def __str__(self):
+        return 'Header: {}\n ' \
+               'PHV: {}\n' \
+               'PORTMASK: {}\n'\
+            .format(_bytestr(self.header),
+                    _bytestr(self.phv),
+                    _bytestr(self.portmask))
