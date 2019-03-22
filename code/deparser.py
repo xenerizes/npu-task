f1b22d5ea41c0c882cf9a7e5401cf00d09ca3cbd
Yuliya Skobtsova <xenerizes@users.noreply.github.com>
2019-03-22 17:48:55 +0300

    ast for deparser

--- code/deparser.py
+++ code/deparser.py
@@ -1,4 +1,5 @@
 from .base import BaseLexer, BaseParser
+from .ast import *
 
 
 class DeparserLexer(BaseLexer):
@@ -25,27 +26,40 @@ class DeparserParser(BaseParser):
     def parse(self, text):
         lexer = DeparserLexer()
         lexer.build()
-        result = self.parser.parse(text, lexer=lexer.lexer)
+        self.build()
+        return self.parser.parse(text, lexer=lexer.lexer)
 
     def p_deparser(self, p):
         'deparser : DPRS code'
+        p[0] = p[2]
 
     def p_code(self, p):
         '''code : empty
                 | load code'''
+        if len(p) == 3:
+            p[0] = Node(p[2], p[1])
+        else:
+            p[0] = None
 
     def p_load(self, p):
         'load : LOAD header COMMA phv COMMA INT'
+        p[0] = TernaryOp('load', p[2], p[4], p[6])
 
     def p_phv(self, p):
         'phv : PHV shift'
+        p[0] = Phv(p[2])
 
     def p_header(self, p):
         'header : HEADER shift'
+        p[0] = Hdr(p[2])
 
     def p_shift(self, p):
         '''shift : empty
                  | PLUS number'''
+        if len(p) == 3:
+            p[0] = p[2]
+        else:
+            p[0] = 0
 
     def p_number(self, p):
         '''number : INT
