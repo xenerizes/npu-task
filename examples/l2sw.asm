e2ace49d47e57e8177f18d26c2491424b081fb72
Yuliya <xenerizes@users.noreply.github.com>
2019-03-23 15:05:13 +0300

    finally swapped src and dst

--- examples/l2sw.asm
+++ examples/l2sw.asm
@@ -4,14 +4,14 @@
     cmpje r1, 0xffffffffffff, halt
 
 .match_action 1
-    mov r1, PHV, 6
+    mov r1, PHV+6, 6
     call exact_match
     cmpje r1, 1, done
     or PORTMASK, 0x80
 done:
 
 .match_action 2
-    mov r1, PHV+6, 6
+    mov r1, PHV, 6
     call exact_match
     cmpje r2, 1, not_found
     or PORTMASK, r1
