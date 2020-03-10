bd971b95c32e6a58cf8a81f62fc656c3611e6ab1
Yuliya Skobtsova <xenerizes@users.noreply.github.com>
2020-03-10 10:44:05 +0300

    multiple instr deparser test

--- test/test_deparser.py
+++ test/test_deparser.py
@@ -6,42 +6,58 @@ from code.ast import *
 class DeparserParserTestCase(TestCase):
     def setUp(self):
         self.parser = DeparserParser()
-        self.ast = None
 
-    def ast_eq(self, v1, v2, v3):
-        self.assertIsInstance(self.ast, Node)
-        self.assertEqual(self.ast.child, None)
-        self.assertIsInstance(self.ast.leaf, TernaryOp)
-        self.assertEqual(self.ast.leaf, TernaryOp('load', Hdr(v1), Phv(v2), v3))
+    def node_eq(self, ast, v1, v2, v3):
+        self.assertIsInstance(ast, Node)
+        self.assertIsInstance(ast.leaf, TernaryOp)
+        self.assertEqual(ast.leaf, TernaryOp('load', Hdr(v1), Phv(v2), v3))
 
     def test_parse_empty(self):
         test_str = "deparser"
-        self.ast = self.parser.parse(test_str)
-        self.assertEqual(self.ast, None)
+        ast = self.parser.parse(test_str)
+        self.assertEqual(ast, None)
 
     def test_parse_one_empty(self):
         test_str = "deparser " \
                    "load HEADER, PHV, 0"
-        self.ast = self.parser.parse(test_str)
-        self.ast_eq(0, 0, 0)
+        ast = self.parser.parse(test_str)
+        self.assertEqual(ast.child, None)
+        self.node_eq(ast, 0, 0, 0)
 
     def test_parse_one_shifted(self):
         test_str = "deparser " \
                    "load HEADER + 2, PHV + 11, 3"
-        self.ast = self.parser.parse(test_str)
-        self.ast_eq(2, 11, 3)
+        ast = self.parser.parse(test_str)
+        self.assertEqual(ast.child, None)
+        self.node_eq(ast, 2, 11, 3)
 
         test_str = "deparser " \
                    "load HEADER + 0, PHV + 0, 5"
-        self.ast = self.parser.parse(test_str)
-        self.ast_eq(0, 0, 5)
+        ast = self.parser.parse(test_str)
+        self.assertEqual(ast.child, None)
+        self.node_eq(ast, 0, 0, 5)
 
         test_str = "deparser " \
                    "load HEADER, PHV + 1, 5"
-        self.ast = self.parser.parse(test_str)
-        self.ast_eq(0, 1, 5)
+        ast = self.parser.parse(test_str)
+        self.assertEqual(ast.child, None)
+        self.node_eq(ast, 0, 1, 5)
 
         test_str = "deparser " \
                    "load HEADER + 7, PHV, 7"
-        self.ast = self.parser.parse(test_str)
-        self.ast_eq(7, 0, 7)
+        ast = self.parser.parse(test_str)
+        self.assertEqual(ast.child, None)
+        self.node_eq(ast, 7, 0, 7)
+
+    def test_parse_multiple(self):
+        test_str = "deparser " \
+                   "load HEADER, PHV, 3 " \
+                   "load HEADER +1, PHV+3, 4 " \
+                   "load HEADER + 6, PHV + 7, 2 "
+        ast = self.parser.parse(test_str)
+        self.assertNotEqual(ast, None)
+        self.assertNotEqual(ast.child, None)
+        self.assertNotEqual(ast.child.child, None)
+        self.node_eq(ast, 0, 0, 3)
+        self.node_eq(ast.child, 1, 3, 4)
+        self.node_eq(ast.child.child, 6, 7, 2)
